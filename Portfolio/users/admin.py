c577cabae49510de5dab8e750158a8d30ad6be77
mechanic <furkalokbu@gmail.com>
2021-05-12 14:46:39 +0300

    add models, admin

--- Portfolio/users/admin.py
+++ Portfolio/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ["username"]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
