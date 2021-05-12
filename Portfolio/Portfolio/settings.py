fa5d5c60cfcc9cdb3b23c4136b5b62ed84318942
mechanic <furkalokbu@gmail.com>
2021-05-12 16:02:09 +0300

    create postgres db and mame migrations

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -67,9 +67,13 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "portfolio",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
